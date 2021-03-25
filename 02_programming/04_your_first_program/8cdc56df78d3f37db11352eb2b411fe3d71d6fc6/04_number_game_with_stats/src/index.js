b17a4c701692bb8a8e204c12f53e025d517d969d
Aboubacar Keita <aboubacarkeita585@gmail.com>
2021-03-25 10:09:11 +0100

    Exo10

--- 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/index.js
+++ 02_programming/04_your_first_program/8cdc56df78d3f37db11352eb2b411fe3d71d6fc6/04_number_game_with_stats/src/index.js
@@ -6,4 +6,20 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
+let europe :string[] = ["Belgique", "France", "Italie", "Allemangne","Espagne"];
+
+console.log("***Welcome to HOLIDAY LOCATION FINDER***");
+console.log(`This program will help you find a location based on the continent you will input.\nHere is the list of 
+continent to chose from:\n
+- Europe\n
+- North America\n
+- South America\n
+- Asia\n
+- Africa\n
+- Oceania`);
+reader.question("Please input your choice:", (choice) =>{
+
+});
+
+
 numberGameWithStats(reader);
