a167cad0ff30f4cedf104e481e3bbb1796bb0a24
Aboubacar Keita <aboubacarkeita585@gmail.com>
2021-04-13 09:46:38 +0200

    Express

---
+++ 03_backend_web_development_and_api/06_express_rest_api/12fd2b5a67b9abdebe409fe15be0f686ab7ac3bb/05_first_api/src/server.ts
@@ -0,0 +1,69 @@
+import express, { Request, Response } from "express";
+import games from "./games.json";
+
+const app = express();
+app.get("/", (request: Request, response: Response) => {
+  //console.log(request.headers);
+  response.status(400);
+  response.json({ error: "Wrong resource" });
+  response.end();
+});
+app.get("/games", (request: Request, response: Response) => {
+  response.status(200);
+  const game = games.map((element) => {
+    return { name: element.platform.name, slug: element.platform.slug, cover: element.platform.platform_logo_url };
+  });
+  response.json(game).end();
+});
+app.get("/games/:game_slug", (request: Request, response: Response) => {
+  const game = games.find((element) => {
+    if (request.params.game_slug === element.slug) {
+      return element;
+    } else {
+      return undefined;
+    }
+  });
+  if (game) {
+    response.status(200).json(game).end();
+  } else {
+    response.status(404).end();
+  }
+});
+type Jeux = {
+  name: string;
+  slug: string;
+  platform_logo_url: string;
+};
+app.get("/platforms/:platform_slug", (request: Request, response: Response) => {
+  const game = games.filter((element) => {
+    if (request.params.platform_slug === element.platform.slug) {
+      return {
+        name: element.platform.name,
+        slug: element.platform.slug,
+        platform_logo_url: element.platform.platform_logo_url,
+      };
+    }
+  });
+  if (game) {
+    response.status(200).json(game).end();
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/platforms", (request: Request, response: Response) => {
+  const game = games.map((element) => {
+    return {
+      name: element.platform.name,
+      slug: element.platform.slug,
+    };
+  });
+  Array.from(new Set(game));
+  if (game) {
+    response.status(200).json(game).end();
+  } else {
+    response.status(404).end();
+  }
+});
+
+export { app };
